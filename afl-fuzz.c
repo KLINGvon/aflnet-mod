b450e822f9f2eea0bba2872a9cca105cd3643440
KLINGvon <978804169@qq.com>
2025-08-26 09:58:20 +0800

    modify choose_target_state

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -255,6 +255,8 @@ static u32 new_paths_since_dot;       /* Paths found since last dot write */
 #define NEW_STATE_BONUS_MULTIPLIER 1.5 // 新状态的得分乘数
 #define RARE_EDGE_BONUS_MULTIPLIER 1.2 // 稀有路径奖励的最大乘数
 
+#define min(a,b) (((a)<(b))?(a):(b))
+
 struct queue_entry {
 
   u8* fname;                          /* File name for the test case      */
